610eddcf774d8b14b6355a1f315dbfcc2cdf38f9
Zentyl <matszc32@wp.pl>
2024-03-17 00:49:59 +0100

    portale nie nachodza juz na siebie

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let table = document.createElement("table");
 table.className = "center";
 grid.appendChild(table);
 
-let cellCounter = 1;
+let cellCounter = 0;
 
 for(let i = 0; i < 7; i++){
     tr = document.createElement("tr");
@@ -20,60 +20,92 @@ for(let i = 0; i < 7; i++){
     }
 }
 
-function createPortals(portal1, portal2, portal3){
+var occupiedCells = [];
+
+function checkCells(portal){
+    for(let i = 0; i < 5; i++){
+        if(occupiedCells.includes(portal[i])){
+            return true;
+        }
+    }
+}
+
+function createHorizontalPortal(portal){
     //Komórki niedostępne dla wylosowanej pozycji ostatniej komórki portalu poziomego
-    let forbidden1 = [];
-    for(let i = 1; i < 50; i+=7){
-        forbidden1.push(i);
+    let forbidden = [];
+    for(let i = 0; i < 49; i+=7){
+        forbidden.push(i);
     }
-    for(let i = 2; i < 50; i+=7){
-        forbidden1.push(i);
+    for(let i = 1; i < 49; i+=7){
+        forbidden.push(i);
     }
+    for(let i = 2; i < 49; i+=7){
+        forbidden.push(i);
+    }
+    let end = 0;
+    do{
+        //Losowanie pozycji ostatniej komórki portalu poziomego
+        do {
+            end = Math.floor(Math.random() * 49);
+        } while(forbidden.includes(end));
 
-    //Losowanie pozycji ostatniej komórki portalu poziomego
-    let pos1 = 0;
-    do {
-        pos1 = Math.floor(Math.random() * 49);
-    } while(forbidden1.includes(pos1));
+        //Utworzenie portalu poziomego
+        portal.push(end - 3, end - 2, end - 1, end);
+    } while(checkCells(portal));
+    occupiedCells.push(...portal);
+}
 
-    //Utworzenie portalu poziomego
-    portal1.push(pos1 - 3, pos1 - 2, pos1 - 1, pos1);
 
+function createVerticalPortal(portal){
     //Komórki niedostępne dla wylosowanej pozycji ostatniej komórki portalu pionowego
-    let forbidden2 = [];
-    for(let i = 1; i < 15; i++){
-        forbidden2.push(i);
+    let forbidden = [];
+    for(let i = 0; i < 21; i++){
+        forbidden.push(i);
     }
-
+    let end = 0;
     //Losowanie pozycji ostatniej komórki portalu pionowego
-    let pos2 = 0;
-    do {
-        pos2 = Math.floor(Math.random() * 49);
-    } while (forbidden2.includes(pos2) || pos2 == pos1);
-    //Utworzenie portalu pionowego
-    portal2.push(pos2 - 21, pos2 - 14, pos2 - 7, pos2);
+    do{
+        portal.length = 0;
+        do {
+            end = Math.floor(Math.random() * 49);
+        } while(forbidden.includes(end));
+        //Utworzenie portalu pionowego
+        portal.push(end - 21, end - 14, end - 7, end);
+    }while(checkCells(portal));
+    occupiedCells.push(...portal);
+}
 
+function createSquarePortal(portal){
     //Komórki niedostępne dla wylosowanej pozycji ostatniej komórki portalu kwadratowego
-    let forbidden3 = [];
-    for(let i = 1; i < 50; i+=7){
-        forbidden3.push(i);
+    let forbidden = [];
+    for(let i = 0; i < 49; i+=7){
+        forbidden.push(i);
     }
-    for(let i = 2; i < 8; i++){
-        forbidden3.push(i);
+    for(let i = 1; i < 7; i++){
+        forbidden.push(i);
     }
-
+    
     //Losowanie pozycji ostatniej komórki portalu kwadratowego
-    let pos3 = 0;
-    do {
-        pos3 = Math.floor(Math.random() * 49);
-    } while (forbidden3.includes(pos3) || pos3 == pos1 || pos3 == pos2);
-    //Utworzenie portalu kwadratowego
-    portal3.push(pos3 - 8, pos3 - 7, pos3 - 1, pos3);
+    let end = 0;
+    do{
+        portal.length = 0;
+        do {
+            end = Math.floor(Math.random() * 49);
+        } while (forbidden.includes(end));
+        //Utworzenie portalu kwadratowego
+        portal.push(end - 8, end - 7, end - 1, end);
+    }while(checkCells(portal));
+    occupiedCells.push(...portal);
 }
 
 let p1 = [];
+createHorizontalPortal(p1);
 let p2 = [];
+createVerticalPortal(p2);
 let p3 = [];
-createPortals(p1, p2, p3);
+createSquarePortal(p3);
+
 
 console.log(p1);
+console.log(p2);
+console.log(p3);
